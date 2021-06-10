4a93015ae92aa95998e4fe821bb024bce4263b8c
Виктор Глушков <viktor@popmechanic.ru>
2021-06-10 19:19:07 +0300

    HOTFIX: review fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,33 @@
-
 let popUpElement = document.querySelector('.pop-up')
 let formElement = popUpElement.querySelector('.edit-form')
 
-let name = document.querySelector('.profile__name')
-let job = document.querySelector('.profile__job')
+let profileName = document.querySelector('.profile__name')
+let profileJob = document.querySelector('.profile__job')
 
-let nameInput = formElement.querySelector('.edit-form__name-input')
-let jobInput = formElement.querySelector('.edit-form__job-input')
+let nameInput = formElement.querySelector('.edit-form__text-input_type_name')
+let jobInput = formElement.querySelector('.edit-form__text-input_type_job')
 
 let closeButton = formElement.querySelector('.edit-form__close-button')
 let editButton = document.querySelector('.profile__edit-button')
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    name.innerHTML = nameInput.value;
-    job.textContent = jobInput.value;
+function closeFormHandler () {
     popUpElement.classList.remove('pop-up_opened');
 }
 
-function closeFormHandler (evt) {
-    popUpElement.classList.remove('pop-up_opened');
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    closeFormHandler()
 }
-function openFormHandler (evt) {
+
+
+function openFormHandler () {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
     popUpElement.classList.add('pop-up_opened');
-    nameInput.value = name.textContent;
-    jobInput.value = job.textContent;
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+
 formElement.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', openFormHandler);
 closeButton.addEventListener('click', closeFormHandler);
